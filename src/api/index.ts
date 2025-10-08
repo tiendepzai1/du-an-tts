e23d04c280d4811968e6e337078c87b83910b13d
le van tien <ngaule29@gmail.com>
2025-10-08 16:04:02 +0700

    commit

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://du-an-tts-fe.onrender.com/user",
+  baseURL: import.meta.env.VITE_API_URL || "https://du-an-tts-fe.onrender.com",
   withCredentials: true, 
 });
 
